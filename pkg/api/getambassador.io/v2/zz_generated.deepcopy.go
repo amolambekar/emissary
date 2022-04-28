e3d5a8e977d09358f8ca1588aeba9025b10cb6ca
AliceProxy <alicewasko@datawire.io>
2022-04-28 10:59:54 -0700

    generate envoy config for health checking
    Signed-off-by: AliceProxy <alicewasko@datawire.io>

--- pkg/api/getambassador.io/v2/zz_generated.deepcopy.go
+++ pkg/api/getambassador.io/v2/zz_generated.deepcopy.go
@@ -21,14 +21,9 @@ package v2
 
 import (
 	"encoding/json"
-<<<<<<< HEAD
 	"github.com/emissary-ingress/emissary/v3/pkg/api/getambassador.io/v3alpha1"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-=======
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
->>>>>>> 6770e47cc (add health checking crds for mappings)
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
